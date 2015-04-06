f0171c31382c43d79fc09cb11e4d19a6d26098d1
Vasilis Vryniotis <bbriniotis@datumbox.com>
2015-04-06 02:41:13 +0100

    Change default random seed to long.

--- src/test/java/com/datumbox/configuration/TestConfiguration.java
+++ src/test/java/com/datumbox/configuration/TestConfiguration.java
@@ -30,7 +30,7 @@ public class TestConfiguration {
     public static final double DOUBLE_ACCURACY_MEDIUM = 0.01;
     public static final double DOUBLE_ACCURACY_LOW = 0.5;
     
-    public static final int RANDOM_SEED = 42;
+    public static final long RANDOM_SEED = 42L;
     
     public static final Class<? extends DatabaseConfiguration> PERMANENT_STORAGE = InMemoryConfiguration.class; 
     //public static final Class<? extends DatabaseConfiguration> PERMANENT_STORAGE = MapDBConfiguration.class; 
