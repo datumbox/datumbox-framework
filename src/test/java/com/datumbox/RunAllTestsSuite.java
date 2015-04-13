49c8809050caf8d900b0fbe221c6ad5b3080b35d
Vasilis Vryniotis <bbriniotis@datumbox.com>
2015-04-13 01:12:16 +0100

    Creation of RunAllTestsSuite.

---
+++ src/test/java/com/datumbox/RunAllTestsSuite.java
@@ -0,0 +1,36 @@
+/**
+ * Copyright (C) 2013-2015 Vasilis Vryniotis <bbriniotis@datumbox.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.datumbox;
+
+import org.junit.extensions.cpsuite.ClasspathSuite;
+import org.junit.extensions.cpsuite.ClasspathSuite.SuiteTypes;
+import org.junit.extensions.cpsuite.SuiteType;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.RunWith;
+
+
+/**
+ * Test Suite that runs all the tests of the project.
+ * 
+ * @author Vasilis Vryniotis <bbriniotis@datumbox.com>
+ */
+@RunWith(ClasspathSuite.class)
+@SuiteTypes({SuiteType.TEST_CLASSES})
+public class RunAllTestsSuite {
+    public static void main(String[] args) {
+        JUnitCore.runClasses(RunAllTestsSuite.class);
+    }
+}
